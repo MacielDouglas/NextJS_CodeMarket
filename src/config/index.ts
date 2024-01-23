dae9dbe3edf51d0cf3736986ce3b5bb2e93dd34c
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-01-23 16:59:17 -0300

    Adicionado navBAr, icones, imagens menu

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: 'UI Kits',
+    value: 'ui_kits' as const,
+    featured: [
+      {
+        name: "Escolha do Editor",
+        href: "#",
+        imageSrc: "/nav/ui-kits/mixed.jpg"
+      },
+      {
+        name: "Novas Entradas",
+        href: "#",
+        imageSrc: "/nav/ui-kits/blue.jpg"
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imageSrc: "/nav/ui-kits/purple.jpg"
+      },
+    ]
+  },
+  {
+    label: 'Ícones',
+    value: 'icons' as const,
+    featured: [
+      {
+        name: "Ícones Favoritos",
+        href: "#",
+        imageSrc: "/nav/icons/picks.jpg"
+      },
+      {
+        name: "Novas Ícones",
+        href: "#",
+        imageSrc: "/nav/icons/new.jpg"
+      },
+      {
+        name: "Bestsellers Icons",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.jpg"
+      },
+    ]
+  },
+]
