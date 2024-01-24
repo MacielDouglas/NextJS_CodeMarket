df7427aee29cc2a7ee6dad6dca6d596b749fb6bd
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-01-24 14:56:28 -0300

    terminado navBar, e criado carrinho de compras

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,3 +1,4 @@
+
 'user client'
 import React from 'react'
 import { Button } from './ui/button'
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "BDT" | 'BRL',
+    notation?: 'compact' | 'standard' | 'scientific' | 'engineering'
+  } = {}
+) {
+  const { currency = 'USD', notation = 'compact' } = options;
+  const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
+
+  const currencyCode = currency === 'BRL' ? 'pt-BR' : 'en-US';
+
+  return new Intl.NumberFormat(currencyCode, {
+    style: 'currency',
+    currency,
+    notation: notation === 'compact' ? 'standard' : notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
+}
+
